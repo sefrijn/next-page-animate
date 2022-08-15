b6ab050f923a047296bca8e0fbe0adf59c00c3e3
How About Yes <sefrijn@gmail.com>
2022-08-15 21:11:54 +0200

    Add variants

--- components/image.js
+++ components/image.js
@@ -1,14 +1,23 @@
 import { motion } from "framer-motion";
 
 export default function Image({ main = false, id }) {
+  //  Conditionally enable or disable the motion props
+  //  Main is true when this Image component is clicked or shown as full size
+  const variants = {
+    initial: !main ? { opacity: 0 } : null,
+    animate: !main
+      ? { opacity: 1, transition: { delay: 0.2 } }
+      : { opacity: 1 },
+    exit: !main ? { opacity: 0 } : null,
+  };
+
   return (
     <motion.div
       layoutId={`wrapper_image_${id}`}
-      initial={!main ? { opacity: 0 } : null}
-      animate={
-        !main ? { opacity: 1, transition: { delay: 0.2 } } : { opacity: 1 }
-      }
-      exit={!main ? { opacity: 0 } : null}
+      variants={variants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
       transition={{ duration: 0.2 }}
       className={"relative w-full h-full"}
     >
